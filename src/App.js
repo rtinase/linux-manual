af4292bd4f135208464516580e98d43354d8174a
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-05-16 08:54:46 +0200

    + fixing exporting of the header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Components/Header';
+import { Header } from './Components/Header';
 
 function App() {
   return (
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,9 @@
 function Header() {
     return (
-        <header class="header">
+        <header>
 
         </header>
     )
 }
+
+export { Header };
