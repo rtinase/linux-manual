06ca515638e59e9b52bffbcf64a61f153ad78d02
Christian-Gasser <christian.gasser@lernende.bbw.ch>
2024-05-16 08:31:21 +0200

    added the two components

---
+++ src/TopicButton.js
@@ -0,0 +1,10 @@
+export default function TopicButton(props) {
+    return (
+        <Link to={props.link}>
+            <div class="card">
+                <h3 class="topic-card-header">{props.title}</h3>
+                <p class="topic-card-descroption">{props.children}</p>
+            </div>
+        </Link>
+    );
+}
