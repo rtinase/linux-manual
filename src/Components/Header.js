c781bc5ec4dfdddfc5eacbee9afa9375553dc1f3
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-05-23 17:09:02 +0200

    + impovements

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,21 @@
+import "./Header.js.css";
+
 function Header() {
     return (
         <header>
-            <img src="./Data/logo.png" alt="Logo" />
+            <div class="logo-block">
+                <div class="logo-section">
+                    <img class="logo" src="./Data/logo.png" alt="Logo"></img>
+                </div>
+                <div class="title-section">
+                    Linux Documentation
+                </div>
+                <div class="search-section">
+                    <img class="search-icon" src="./Data/search.png" alt="Logo"></img>
+                    <p>Search</p>
+                </div>
+            </div>
+
         </header>
     )
 }
