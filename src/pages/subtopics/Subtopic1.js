577821ab1c805d0e51e0f1901a6042c4a1adc128
faboulos008 <faboulos007@yahoo.com>
2024-06-06 16:50:51 +0200

    removed BreadCrumb

--- src/pages/subtopics/Subtopic1.js
+++ src/pages/subtopics/Subtopic1.js
@@ -1,11 +1,9 @@
 // Beispielsweise in src/pages/subtopics/Subtopic1.js
 import React from 'react';
-import Breadcrumbs from '../../Components/Breadcrumbs.js';
 
 const Subtopic1 = () => {
   return (
     <div>
-      <Breadcrumbs />
       <h1>Subtopic 1</h1>
       <p>This is the detailed page for Subtopic 1.</p>
     </div>
