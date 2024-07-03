a880ca564c39bbd784bb22d071a6a926cb1015aa
Roman Berli <roman.berli2007@gmail.com>
2024-07-03 11:33:14 +0200

    Added Content about Distributionen und Geschichte.

---
+++ src/pages/undersubtopics/Debian-Linux.js
@@ -0,0 +1,25 @@
+import ArticlePara from "../../Components/ArticlePara";
+import ArticleTitle from '../../Components/ArticleTitle';
+
+const Debian = () => (
+  <>
+    <ArticleTitle>Debian</ArticleTitle>
+    <ArticlePara>
+      Debian ist eine der ältesten und bekanntesten Linux-Distributionen. Sie wurde 1993 von Ian Murdock gegründet und zeichnet sich durch ihre Stabilität und umfangreiche Software-Repositories aus. Debian verfolgt eine strikte Einhaltung der Prinzipien freier Software, was bedeutet, dass alle Softwarepakete, die in den offiziellen Repositories enthalten sind, frei von proprietären Komponenten sind.
+    </ArticlePara>
+    <ArticlePara>
+      Eine der größten Stärken von Debian ist seine Stabilität. Neue Versionen von Debian durchlaufen einen rigorosen Testprozess, bevor sie als „stable“ freigegeben werden. Dies macht Debian zu einer idealen Wahl für Server und andere kritische Anwendungen, bei denen Zuverlässigkeit und Langzeitunterstützung wichtig sind.
+    </ArticlePara>
+    <ArticlePara>
+      Debian dient als Basis für viele andere beliebte Distributionen, darunter Ubuntu, Linux Mint und viele andere. Dies zeigt die Flexibilität und Anpassungsfähigkeit des Debian-Systems. Das Debian-Projekt wird vollständig von Freiwilligen betrieben und hat eine große und aktive Community, die zur Weiterentwicklung und Unterstützung der Distribution beiträgt.
+    </ArticlePara>
+    <ArticlePara>
+      Die Paketverwaltung in Debian erfolgt über das Advanced Packaging Tool (APT), das die Installation, Aktualisierung und Entfernung von Softwarepaketen erleichtert. Debian bietet auch eine Vielzahl von Desktop-Umgebungen an, darunter GNOME, KDE, Xfce und viele andere, um den unterschiedlichen Bedürfnissen und Vorlieben der Benutzer gerecht zu werden.
+    </ArticlePara>
+    <ArticlePara>
+      Insgesamt ist Debian eine vielseitige und robuste Distribution, die sich sowohl für Anfänger als auch für erfahrene Benutzer eignet. Ihre lange Geschichte und die breite Unterstützung durch die Community machen sie zu einer der solidesten und vertrauenswürdigsten Linux-Distributionen.
+    </ArticlePara>
+  </>
+);
+
+export default Debian;
--- src/routes.js
+++ src/routes.js
@@ -8,13 +8,25 @@ import Subtopic1 from './pages/subtopics/Subtopic1';
 import Subtopic2 from './pages/subtopics/Subtopic2';
 import Subtopic3 from './pages/subtopics/Subtopic3';
 import Undersubtopic1 from './pages/undersubtopics/Undersubtopic1';
+
+//Benutzer
 import Useradd from './pages/undersubtopics/Useradd';
 import Adduser from './pages/undersubtopics/Adduser';
 import Createuser from './pages/subtopics/Createuser';
 import Usermod from './pages/subtopics/Usermod'
 import Userdel from './pages/subtopics/Userdel';
 import User from './pages/topics/User';
+
+//Direktories und Geschichte
 import Directory from './pages/topics/Directory';
+import DasErsteLinuxBetriebssystem from './pages/subtopics/First-Linux';
+import LinusTorvalds from './pages/subtopics/Linus-Torvalds';
+import Distributionen from './pages/subtopics/Distributionen';
+import ArchLinux from './pages/undersubtopics/Arch-Linux';
+import Debian from './pages/undersubtopics/Debian-Linux';
+import Kali from './pages/undersubtopics/Kali-Linux';
+import Mint from './pages/undersubtopics/Mint-Linux';
+import Ubuntu from './pages/undersubtopics/Ubuntu-Linux';
 
 const RoutesConfig = () => (
   <React.Fragment>
@@ -25,14 +37,25 @@ const RoutesConfig = () => (
     <Route path="/subtopics/subtopic3" element={<Subtopic3 />} />
     <Route path="/undersubtopics/undersubtopic1" element={<Undersubtopic1 />} />
     <Route path="/topics/topic2" element={<Topic2 />} />
-    <Route path="/undersubtopics/useradd" element={<Useradd />} />
-    <Route path="/undersubtopics/adduser" element={<Adduser />} />
-    <Route path="/subtopics/createuser" element={<Createuser />} />
+    
+    <Route path="/topics/user" element={<User />} />
     <Route path="/subtopics/usermod" element={<Usermod />} />
     <Route path="/subtopics/userdel" element={<Userdel />} />
-    <Route path="/topics/user" element={<User />} />
+    <Route path="/subtopics/createuser" element={<Createuser />} />
+    <Route path="/undersubtopics/useradd" element={<Useradd />} />
+    <Route path="/undersubtopics/adduser" element={<Adduser />} />
+    
     <Route path="/topics/directory" element={<Directory />} />
+    <Route path="/subtopics/Linus-Torvalds" element={<LinusTorvalds />} />
+    <Route path="/subtopics/erstes-Linux" element={<DasErsteLinuxBetriebssystem />} />
+    <Route path="/subtopics/Distributionen" element={<Distributionen />} />
+    <Route path="/undersubtopics/Debian" element={<Debian />} />
+    <Route path="/undersubtopics/Ubuntu" element={<Ubuntu />} />
+    <Route path="/undersubtopics/Mint" element={<Mint />} />
+    <Route path="/undersubtopics/Kali" element={<Kali />} />
+    <Route path="/undersubtopics/Arch" element={<ArchLinux />} />
 
+    
   </React.Fragment>
 );
 
