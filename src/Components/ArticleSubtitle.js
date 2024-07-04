43c7eff546cb9310ca7233ee3e32de011f8aaa78
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-07-04 14:56:24 +0200

    Updates

--- src/Components/ArticleSubtitle.js
+++
@@ -1,9 +0,0 @@
-import './Article.css';
-
-export default function ArticleSubtitle(props) {
-    return (
-        <div>
-            <h2 className="article-subtitle">{ props.children }</h2>
-        </div>
-    )
-}
--- src/Components/ArticleTitle.js
+++
@@ -1,9 +0,0 @@
-import './Article.css';
-
-export default function ArticleTitle(props) {
-    return (
-        <div>
-            <h1 className="article-title">{ props.children }</h1>
-        </div>
-    )
-}
